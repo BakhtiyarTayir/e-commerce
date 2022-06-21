b23c3730896a6efcd865f1b9180a5f6a067c42c7
BakhtiyarTayir <BakhtiyarTayir@gmail.com>
2022-06-21 08:57:14 +0500

    refactoring

--- core/store/models.py
+++ core/store/models.py
@@ -1,8 +1,12 @@
-from tabnanny import verbose
+from email.policy import default
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
 
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(in_active=True)
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
@@ -17,21 +21,25 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
-
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    category = models.ForeignKey(Category, 
+                                    related_name='products', 
+                                    on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, 
+                                    on_delete=models.CASCADE, 
+                                    related_name='product_creator')
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
     author = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     in_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
         verbose_name_plural = 'Products'
